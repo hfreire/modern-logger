e38a5c35290fc9300359ba9ad08d3ae9bb02cff6
Hugo Freire <hfreire@exec.sh>
2017-04-01 17:52:59 +0200

    test: upgrade tests to testdouble 2.1.2

--- test/modern-logger.js
+++ test/modern-logger.js
@@ -7,131 +7,112 @@
 
 describe('Modern Logger', () => {
   let subject
-  let Logger
   let winston
+  let RollbarTransport
   let emoji
 
-  afterEach(() => {
-    td.reset()
+  before(() => {
+    winston = td.object([ 'transports' ])
+    winston.transports.Console = td.constructor()
+    winston.Logger = td.constructor([ 'debug', 'info', 'warn', 'error' ])
+
+    RollbarTransport = td.constructor()
+
+    emoji = td.object([ 'get' ])
   })
+
+  afterEach(() => td.reset())
 
   describe('when logging an debug message', () => {
     const message = 'my-message'
 
-    before(() => {
-      Logger = td.object([ 'debugAsync', 'infoAsync', 'warnAsync', 'errorAsync' ])
-
-      td.replace(require('winston'), 'Logger', Logger)
+    beforeEach(() => {
+      td.replace('winston', winston)
 
       subject = require('../src/modern-logger')
-    })
-
-    after(() => {
-      delete require.cache[ require.resolve('../src/modern-logger') ]
     })
 
     it('should invoke winstons debug logger', () => {
       subject.debug(message)
 
-      td.verify(Logger.debugAsync(message), { times: 1 })
+      td.verify(winston.Logger.prototype.debug(message), { ignoreExtraArgs: true, times: 1 })
     })
   })
 
   describe('when logging an info message', () => {
     const message = 'my-message'
 
-    before(() => {
-      Logger = td.object([ 'debugAsync', 'infoAsync', 'warnAsync', 'errorAsync' ])
-
-      td.replace(require('winston'), 'Logger', Logger)
+    beforeEach(() => {
+      td.replace('winston', winston)
 
       subject = require('../src/modern-logger')
-    })
-
-    after(() => {
-      delete require.cache[ require.resolve('../src/modern-logger') ]
     })
 
     it('should invoke winstons info logger', () => {
       subject.info(message)
 
-      td.verify(Logger.infoAsync(message), { times: 1 })
+      td.verify(winston.Logger.prototype.info(message), { ignoreExtraArgs: true, times: 1 })
     })
   })
 
   describe('when logging a warning message', () => {
     const message = 'my-message'
 
-    before(() => {
-      Logger = td.object([ 'debugAsync', 'infoAsync', 'warnAsync', 'errorAsync' ])
-
-      td.replace(require('winston'), 'Logger', Logger)
+    beforeEach(() => {
+      td.replace('winston', winston)
 
       subject = require('../src/modern-logger')
-    })
-
-    after(() => {
-      delete require.cache[ require.resolve('../src/modern-logger') ]
     })
 
     it('should invoke winstons warning logger', () => {
       subject.warn(message)
 
-      td.verify(Logger.warnAsync(message), { times: 1 })
+      td.verify(winston.Logger.prototype.warn(message), { ignoreExtraArgs: true, times: 1 })
     })
   })
 
   describe('when logging an error message', () => {
     const message = 'my-message'
 
-    before(() => {
-      Logger = td.object([ 'debugAsync', 'infoAsync', 'warnAsync', 'errorAsync' ])
-
-      td.replace(require('winston'), 'Logger', Logger)
+    beforeEach(() => {
+      td.replace('winston', winston)
 
       subject = require('../src/modern-logger')
-    })
-
-    after(() => {
-      delete require.cache[ require.resolve('../src/modern-logger') ]
     })
 
     it('should invoke winstons error logger', () => {
       subject.error(message)
 
-      td.verify(Logger.errorAsync(message), { times: 1 })
+      td.verify(winston.Logger.prototype.error(message), { ignoreExtraArgs: true, times: 1 })
     })
   })
 
   describe('when Rollbar API key available', () => {
-    let Console
-    let Transport
+    let winston
 
-    before(() => {
+    beforeEach(() => {
       process.env.ROLLBAR_API_KEY = 'my-rollbar-api-key'
 
-      Console = td.function()
-      Logger = td.function()
-      Transport = td.function()
-      winston = td.replace('winston', { Logger, Transport, transports: { Console } })
+      winston = td.replace('winston', {
+        Logger: td.function(),
+        transports: { Console: td.function() }
+      }) // can't seem to test constructor
+
+      td.replace('../src/rollbar-transport', RollbarTransport)
 
       subject = require('../src/modern-logger')
     })
 
-    after(() => {
-      delete require.cache[ require.resolve('../src/modern-logger') ]
-
-      delete process.env.ROLLBAR_API_KEY
-    })
+    after(() => delete process.env.ROLLBAR_API_KEY)
 
     it('should create logger with Rollbar transport', () => {
       const captor = td.matchers.captor()
 
-      td.verify(winston.Logger(captor.capture()), { times: 1 })
+      td.verify(winston.Logger(captor.capture()), { times: 1 }) // can't seem to test constructor
 
       const options = captor.value
 
-      options.should.have.deep.property('transports[1].name', 'rollbar')
+      options.transports.should.have.length(2)
     })
   })
 
@@ -140,18 +121,12 @@ describe('Modern Logger', () => {
     let emojiCode = `:${emojiId}:`
     const message = `my-${emojiCode}-message`
 
-    before(() => {
-      Logger = td.object([ 'debugAsync', 'infoAsync', 'warnAsync', 'errorAsync' ])
+    beforeEach(() => {
+      td.replace('winston', winston)
 
-      td.replace(require('winston'), 'Logger', Logger)
-
-      emoji = td.replace('node-emoji', td.object([ 'get' ]))
+      td.replace('node-emoji', emoji)
 
       subject = require('../src/modern-logger')
-    })
-
-    after(() => {
-      delete require.cache[ require.resolve('../src/modern-logger') ]
     })
 
     it('should invoke emoji with emoji id', () => {
